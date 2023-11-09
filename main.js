90ea05daefbb9bbae68a1df2225c4b894713d0d3
LGoulart <leandrovgoulart@gmail.com>
2023-11-09 01:22:45 -0300

    create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case "mago":
+                ataque = "magia";
+                break;
+            case "guerreiro":
+                ataque = "espada";
+                break;
+            case "monge":
+                ataque = "artes marciais";
+                break;
+            case "ninja":
+                ataque = "shuriken";
+                break;
+            default:
+                ataque = "um ataque desconhecido";
+        }
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+const gandalf = new Heroi("Gandalf", 1000, "mago");
+
+const kennen = new Heroi("Kennen", 135, "ninja");
+
+gandalf.atacar();
+
+kennen.atacar();
